ed85f43508b618b613377e1dc5582726705df01d
sadministrator <marcothewebdev@gmail.com>
2023-10-19 22:43:55 -0600

    implement bidirectional test

---
+++ bidirectional/configs/hosts/charlie/return.sh
@@ -0,0 +1,10 @@
+# A received message, in the form 'b"{message}" from {sender}'
+received = $(earendil control recv-message)
+
+# Extract message
+message=$(echo "$received" | cut -d'"' -f2)
+
+# Extract sender
+sender=$(echo "$received" | awk '{print $NF}')
+
+earendil control send-message --destination $sender --message $message
