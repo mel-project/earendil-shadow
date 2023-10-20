7d5c4e9468d474060d62fb81d47eb6640a8701fd
sadministrator <marcothewebdev@gmail.com>
2023-10-20 12:55:17 -0600

    implements anon-echo test

--- bidirectional/configs/hosts/charlie/return.sh
+++ bidirectional/configs/hosts/charlie/return.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+# Workaround to add users' local cargo binaries to path
+for dir in /home/*/ ; do
+    PATH=$PATH:"$dir".cargo/bin
+done
+
 # A received message, in the form 'b"{message}" from {sender}'
-received = $(earendil control recv-message)
+received=$(earendil control recv-message)
 
 # Extract message
 message=$(echo "$received" | cut -d'"' -f2)
 
 # Extract sender
-sender=$(echo "$received" | awk '{print $NF}')
+sender=$(echo "$received" | rev | cut -d' ' -f1 | rev)
 
 earendil control send-message --destination $sender --message $message
