2d249c7654434d80e57e889161e6af1da74c0439
thisbefruit <thisbefruit@protonmail.com>
2023-10-28 12:13:25 -0400

    added tests for global rpc & dht

--- anon-echo/run-test.sh
+++ anon-echo/run-test.sh
@@ -1,14 +1,13 @@
+#!/bin/bash
+
 # run shadow test
 rm -rf shadow.data && shadow shadow.yaml;
 
-# check shadow.data to see if bob received alice's message
-#!/bin/bash
-
-# Define the directory and the string to be searched
+# Define the file and the string to be searched
 FILE_PATH="./shadow.data/hosts/alice/earendil.1010.stdout"
 SEARCH_STRING="hellome"
 
-# Check each file in the directory for the search string
+# Check the file for the search string
 if grep -q "$SEARCH_STRING" "$FILE_PATH"; then
     echo "PASSED"
 else
--- global-rpc/run-test.sh
+++ global-rpc/run-test.sh
@@ -4,8 +4,8 @@
 rm -rf shadow.data && shadow shadow.yaml;
 
 # Define the file and the string to be searched
-FILE_PATH="./shadow.data/hosts/charlie/earendil.1004.stdout"
-SEARCH_STRING="hellocharlie"
+FILE_PATH="./shadow.data/hosts/eric/earendil.1010.stdout"
+SEARCH_STRING="12345"
 
 # Check the file for the search string
 if grep -q "$SEARCH_STRING" "$FILE_PATH"; then
