25ddf6a48e9408506f03a618e7cd316608e980ea
antidmg <antidmg@proton.me>
2023-11-30 13:04:57 -0600

    TCP forwarding works

--- stream/run-test.sh
+++ stream/run-test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # run shadow test
-rm -rf shadow.data && shadow shadow.yaml;
+rm -rf shadow.data && shadow shadow.yaml
 
 # Define the file and the string to be searched
-FILE_PATH="./shadow.data/hosts/bob/earendil.1010.stdout"
+FILE_PATH="./shadow.data/hosts/bob/bash.1001.stdout"
 SEARCH_STRING="the future is now"
 
 # Check the file for the search string
-if grep -q "$SEARCH_STRING_1" "$FILE_PATH_1" && grep -q "$SEARCH_STRING_2" "$FILE_PATH_2"; then
-    echo "PASSED"
+if grep -q "$SEARCH_STRING" "$FILE_PATH"; then
+	echo "PASSED"
 else
-    echo "FAILED"
+	echo "FAILED"
 fi
